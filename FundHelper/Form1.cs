42bfdad69ebcfaedba7452f223b09a18786cd554
wnmwi <383929371@qq.com>
2019-11-21 11:51:45 +0800

    初始化

---
+++ FundHelper/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FundHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            long jsTimeNow = GetJsTimestampNow();
+            string getUrl = $"http://hq.sinajs.cn/rn={jsTimeNow}&list=s_sh000001,s_sh000002,s_sh000003,s_sh000004,s_sh000005,s_sh000006,s_sh000007,s_sh000008,s_sh000009";
+            byte[] buffer = SimpleGet(getUrl);
+            string result = Encoding.Default.GetString(buffer);
+        }
+
+        /// <summary>
+        /// 简易HttpGet请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public byte[] SimpleGet(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            int count = (int)response.ContentLength;
+            int offset = 0;
+            byte[] buf = new byte[count];
+            while (count > 0)
+            { //循环度可以避免数据不完整
+                int n = stream.Read(buf, offset, count);
+                if (n == 0) break;
+                count -= n;
+                offset += n;
+            }
+            return buf;
+        }
+
+        private long GetJsTimestampNow()
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1)); // 当地时区
+            long timeStamp = (long)(DateTime.Now - startTime).TotalMilliseconds; // 相差毫秒数
+            return timeStamp;
+        }
+    }
+}
