635a3454fc8a56b62e93ce8cb91221ced30b59ad
wnmwi <383929371@qq.com>
2019-12-11 00:35:03 +0800

    第一版本

---
+++ FundHelper/Found.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundHelper
+{
+    public class Found
+    {
+        public string Code { get; set; } // 基金编码
+        public string Name { get; set; } //基金名称
+        public double? realIncrease { get; set; } //实时涨幅
+
+    }
+}
